22e5bca963e920a0e21a26c0c263ac7e5294bcda
Chris Williams <chrisfwilliams@gmail.com>
2018-01-04 18:51:34 -0500

    test

--- InventYourOwnComputerGamesPython/Chap10/tictactoe.py
+++ InventYourOwnComputerGamesPython/Chap10/tictactoe.py
@@ -3,7 +3,7 @@
 import random
 
 def drawBoard(board):
-    # This function prints out the board that it was passed.   
+    # This function prints out the board that it was passed.
 
     # "board" is a list of 10 strings representing the board (ignore index 0).
     print(board[7] + '|' + board[8] + '|' + board[9])
