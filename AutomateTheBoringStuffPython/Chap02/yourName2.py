ea616748b62538d4b624097337e3be6da9f30447
Chris Williams <chrisfwilliams@gmail.com>
2018-01-14 19:00:38 -0500

    asdf

---
+++ AutomateTheBoringStuffPython/Chap02/yourName2.py
@@ -0,0 +1,8 @@
+name = ''
+while True:  # Creates an infinite loop
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break # Breaks the infinite loop
+print("Thank you!")
+
