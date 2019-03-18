8a07920cc1aa91e369c9a17a6911e506d5000c62
Chris Williams <chrisfwilliams@gmail.com>
2019-03-17 21:29:33 -0400

    sync with new workstation

---
+++ ModernPythonBootcamp/coin_flip.py
@@ -0,0 +1,11 @@
+#Coin Toss Function
+from random import random
+
+def flip_coin():
+    # generate a random number 0-1
+    if random() > 0.5:
+        return 'Heads'
+    else:
+        return 'Tails'
+
+print(flip_coin())
---
+++ ModernPythonBootcamp/scratch.py
@@ -0,0 +1,24 @@
+# def sing_happy_birthday(name):
+#     print("Happy Birthday To You")
+#     print("Happy Birthday To You")
+#     print(f"Happy Birthday Dear {name}")
+#     print("Happy Birthday To You")
+
+# sing_happy_birthday('Chris')
+#------------------------------------------------
+# def square_of_seven():
+#     print('I am before the return')
+#     return 7**2
+#     print('I am after the return') # this never gets printed because the return exits the function
+
+# result = square_of_seven()
+# print(result)
+#----------------------------------------------
+
+# def generate_evens():
+#     return [x for x in range(1,50) if x%2 == 0]
+
+# print(generate_evens())
+
+#----------------------------------------------
+
