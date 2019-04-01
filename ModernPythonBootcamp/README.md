1a16ae606fe33457da0c7fab5bb12d6b1a32cad3
Chris Williams <chrisfwilliams@gmail.com>
2019-04-01 09:01:55 -0400

    add readme to Colt Steele course

---
+++ ModernPythonBootcamp/README.md
@@ -0,0 +1,5 @@
+The Modern Python 3 Bootcamp
+
+Notes and such from the udemy class from Colt Steele
+
+https://www.udemy.com/the-modern-python3-bootcamp 
--- ModernPythonBootcamp/single_letter_count.py
+++ ModernPythonBootcamp/single_letter_count.py
@@ -8,5 +8,4 @@ single_letter_count("HelLo World", "l") # 3
 def single_letter_count(string1, string2):
     return string1.lower().count(string2.lower())
 
-
 print(single_letter_count('Hello World', 'h'))
