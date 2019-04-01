17ffd9146279f73f4b2634619312037eba243f2a
Chris Williams <chrisfwilliams@gmail.com>
2019-03-31 21:33:37 -0400

    more studying

---
+++ ModernPythonBootcamp/multiple_letter_count.py
@@ -0,0 +1,9 @@
+'''
+multiple_letter_count("awesome") # {'a': 1, 'e': 2, 'm': 1, 'o': 1, 's': 1, 'w': 1}
+'''
+
+# flesh out multiple_letter count:
+def multiple_letter_count(a_string):
+    for x in x 
+
+    pass
