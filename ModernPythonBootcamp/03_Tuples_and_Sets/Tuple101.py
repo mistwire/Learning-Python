8402742837b71d1ed1071b02e530dcd01b38093d
Chris Williams <chrisfwilliams@gmail.com>
2019-04-04 15:09:20 -0400

    moar studeez

---
+++ ModernPythonBootcamp/03_Tuples_and_Sets/Tuple101.py
@@ -0,0 +1,9 @@
+# immutable, ordered collection or grouping of items
+
+my_1st_tuple = (1, 'a', 3, 4)
+
+x = (1, 2, 3)
+print(3 in x)  # True
+
+
+# tuples are faster than lists because they don't change
