92aa9616900512e44d73c6a1b9c3cd4ed50b155f
Chris Williams <chrisfwilliams@gmail.com>
2019-05-29 22:37:25 -0400

    May 29 notes

--- 01_modern_python_bootcamp/05_lambdas_and_built_in_functions/09_abs_sum_round.py
+++ 01_modern_python_bootcamp/05_lambdas_and_built_in_functions/09_abs_sum_round.py
@@ -18,3 +18,41 @@ print(sum([12.23,23.12,54,23]))
 print(sum([12.23,23.12,54,23], -112))
 
 
+# round returns a rounded version, specify a number of digits for decimal, integer if none specified
+
+print(round(3.51234))  #rounds to an integer
+
+print(round(3.51234, 3)) # rounds to 3 decimal places
+
+print(round(3.51234, None)) # rounds to an integer
+
+
+# Write a function that accepts a single list full of numbers & returns the magnitude of the number with the largest magnitude:
+# max_magnitude([300, 20, -900])  ->  returns 900
+# max_magnitude([10,11,12])  -> returns 12
+# max_magnitude([-5, -1, -89])  -> returns 89
+
+def max_magnitude(l1):
+    l2 = [abs(num) for num in l1]
+    return max(l2)
+    
+def max_magnitude(l1):
+    return max([abs(num) for num in l1])
+
+def max_magnitude(l1):
+    return max(abs(num) for num in l1)
+
+# Write a function called sum_floats. 
+# This function should accept a variable number of arguments. 
+# The function should return the sum of all the parameters that are floats. 
+# If there are no floats the function should return 0
+
+def sum_floats(*args):
+    total = 0
+    for num in args:
+        if isinstance(num, float):
+            total += num
+    return total
+
+def sum_floats(*args):
+    return sum(arg for arg in args if type(arg) == float)
