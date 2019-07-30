7b6b592cc94076f0b7267b4ebba277b395595d5a
Chris Williams <chrisfwilliams@gmail.com>
2019-07-29 21:16:24 -0400

    notes

---
+++ 01_modern_python_bootcamp/09_OOP/00_person.py
@@ -0,0 +1,15 @@
+# The simplest class possible
+
+class User: #empty shell of a class that doesn't do anything
+    pass 
+
+# need to call it just like you would a function
+user1 = User()
+user2 = User()
+user3 = User()
+user4 = User()
+
+print(user1)
+print(user2) # different empty user (you can tell when you print it - diff memory location)
+print(type(user1))
+
--- 01_modern_python_bootcamp/09_OOP/01_init.py
+++ 01_modern_python_bootcamp/09_OOP/01_init.py
@@ -1,14 +1,19 @@
+# when you create a class, python looks for __init__ method, 
+# which gets called every time you create an instance of the class (when you instantiate it)
+
+
 class User:
-    def __init__(self, first, last="default", age=46):
-        self.first = first
-        self.last = last
+    def __init__(self, first, last="default", age=46): # same rules apply as with defaul parameters
+        # here is where you initialize the data that each new user has
+        self.first = first # self refers to the current instance of the User class 
+        self.last = last # assignments don't need to be named the same 
         self.age = age
 
-user1 = User("Joe")
+user1 = User("Joe", "Smith", 68)
 user2 = User("Blanca")
 
-print(user1.first, user1.last)
-print(user2.age)
+print(user1.first, user1.last, user1.age)
+print(user2.first, user2.last, user2.age)
 
 
 
