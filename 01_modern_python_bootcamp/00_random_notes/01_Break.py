a2fa55423221d68f70fdb3801998b8b64cceb5da
Chris Williams <chrisfwilliams@gmail.com>
2019-08-01 21:50:30 -0400

    test

--- 01_modern_python_bootcamp/00_random_notes/01_Break.py
+++ 01_modern_python_bootcamp/00_random_notes/01_Break.py
@@ -1,5 +1,5 @@
 # The break statement will completely break out of the current loop, meaning it won’t run any more of the statements contained inside of it.
-names = ["Max", "Rose", "Nina", "Chris"]     
+names = ["Max", "Rose", "Nina", "Chris"]
 for name in names:
     print(f"Hello, {name}")
     if name == "Nina":
