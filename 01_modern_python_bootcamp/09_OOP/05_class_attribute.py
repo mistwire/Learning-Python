aab2767339f12e46309d2728496902aa64b02408
Chris Williams <chrisfwilliams@gmail.com>
2019-08-03 16:03:37 -0400

    moar notez

--- 01_modern_python_bootcamp/09_OOP/05_class_attribute.py
+++ 01_modern_python_bootcamp/09_OOP/05_class_attribute.py
@@ -11,23 +11,31 @@ class User:
         self.age = age
         User.active_users += 1 # this will increment active_users any time a User is initialized now.
 
-# methods are written just like regular functions... but they become methods when written inside of a class
-    def full_name(self): # difference - still need to provide self
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out"
+
+    def full_name(self): 
         return f"{self.first} {self.last}"
 
-    def initials(self): # self will *always* be the 1st parameter in any instance method
+    def initials(self): 
         return f"{self.first[0]}.{self.last[0]}"
 
     def likes(self, thing):
         return f"{self.first} likes {thing}"
 
     def is_senior(self):
-        return self.age >= 65 # this is a boolean expression that will return True or False
+        return self.age >= 65 
 
     def birthday(self):
         self.age += 1
         return f"Happy {self.age}th, {self.first}"
 
 
+print(User.active_users)
+user1 = User("Joe", "Smith", 68)
+user2 = User("Blanca", "Lopez", 41)
+print(User.active_users)
+print(user2.logout())
 print(User.active_users)
 
